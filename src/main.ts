5d0f72713621994a5a522dcabcc350f52eee0d2c
rdupiereuxDISID <rdupiereux@disid.com>
2025-03-19 18:31:34 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.css';
+
+// import './topics/01-basic-types'
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = 'Hola Mudo';
---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,11 @@
+const name: string = 'Gusmin';
+let hpPoints: number | 'FULL' = 95;
+const isAlive: boolean = true;
+
+hpPoints = 'FULL';
+
+console.log({
+    name, hpPoints, isAlive
+})
+
+export{}
