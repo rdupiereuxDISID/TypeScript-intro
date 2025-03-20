fc69608b231adb23fe47b893fbca81f460839407
rdupiereuxDISID <rdupiereux@disid.com>
2025-03-20 17:14:00 +0100

    Clase de clases en TS - parte 2

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -4,13 +4,36 @@ export class Cat {
     //public name: string;
     //private address: string;
 
-    constructor( public name: string, private address: string = 'No Address' ) {
-        this.name = name;
-        this.address = address;
-    }
+    constructor( public name: string, private address: string = 'No Address' ) {}
 
 }
 
-const ironcat = new Cat ( 'Jünger', 'Valencia');
+// Concepto de extender clases en TS.
+/*
+export class Hero extends Cat {
+    constructor( 
+        public alterEgo: string, 
+        public age: number, 
+        public realName: string, 
+    ) {
+        super( realName, 'Valencia' )
+    }
+}
+*/
+
+export class Hero {
+
+    constructor( 
+        public alterEgo: string, 
+        public age: number, 
+        public realName: string,
+        public cat: Cat,
+    ) {
+        //this.cat = new Cat(realName)
+    }
+}
+
+const cat = new Cat ( 'Jünger', 'Valencia' );
+const ironcat = new Hero ( 'Gusmin', 1, 'Jünger', cat );
 
 console.log(ironcat);
